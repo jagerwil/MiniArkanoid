62a5ceb241ab753076b6000586bcfd741c4b70db
jagerwil <31615013+jagerwil@users.noreply.github.com>
2025-11-30 18:40:43 +0300

    Added graphics & stone brick (requires 2 hits to destroy)

--- Assets/Scripts/Gameplay/Bricks/Brick.cs
+++ Assets/Scripts/Gameplay/Bricks/Brick.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Game.Gameplay._Services;
 using UnityEngine;
 using Zenject;
@@ -7,6 +9,9 @@ namespace Game.Gameplay.Bricks {
     public class Brick : MonoBehaviour, IDamageable {
         [SerializeField] private float _maxHealth;
         [SerializeField] private int _score;
+        [Space]
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private List<BrickHealthGraphics> _graphics;
         
         [Inject] private IScoreService _scoreService;
 
@@ -14,21 +19,24 @@ namespace Game.Gameplay.Bricks {
 
         private Action _destroyedCallback;
 
-        private void Awake() {
-            _health = _maxHealth;
-        }
-
         public void Initialize(Action destroyedCallback) {
             _destroyedCallback = destroyedCallback;
         }
+        
+#if UNITY_EDITOR
+        //Dynamically update the graphics in the editor
+        private void OnValidate() {
+            SetHealth(_maxHealth);
+        }
+#endif
 
         public void Restore() {
-            _health = _maxHealth;
+            SetHealth(_maxHealth);
             gameObject.SetActive(true);
         }
 
         public void TakeDamage(float damage) {
-            _health -= damage;
+            SetHealth(_health - damage);
             if (_health > 0f) {
                 return;
             }
@@ -37,5 +45,27 @@ namespace Game.Gameplay.Bricks {
             _scoreService.ChangeScore(_score);
             _destroyedCallback?.Invoke();
         }
+
+        private void SetHealth(float health) {
+            _health = Mathf.Max(health, 0f);
+            var healthPercent = _health / _maxHealth;
+            
+            var graphicsInfo = _graphics.LastOrDefault(graphics => graphics.StartHealthPercent >= healthPercent);
+            if (graphicsInfo == null) {
+                Debug.LogError($"Could not find the graphics for health percent {healthPercent}");
+                return;
+            }
+            
+            _spriteRenderer.sprite = graphicsInfo.Sprite;
+            _spriteRenderer.color = graphicsInfo.Color;
+        }
+    }
+
+    [Serializable]
+    public class BrickHealthGraphics {
+        [field: Range(0f, 1f)]
+        [field: SerializeField] public float StartHealthPercent { get; private set; }
+        [field: SerializeField] public Sprite Sprite { get; private set; }
+        [field: SerializeField] public Color Color { get; private set; }
     }
 }
